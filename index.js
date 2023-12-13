61d1576f4a7e5a19ba69a8d624653a4329316545
Gustavo <gumarinho090306@gmail.com>
2023-12-13 14:20:46 -0300

    Add express and configure middleware

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+const express = require('express');
 require('dotenv').config();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log('Connected to MongoDB');
-        // Start your server or perform other operations here
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+}).then(() => {
+    console.log('Connected to MongoDB');
+    // Start your server or perform other operations here
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+const app = express();
+app.use(express.json());
